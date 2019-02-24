9fda5001922d2f153c1aad405574fa27d43271da
Allen B <pingguo.ny@gmail.com>
2019-02-24 15:05:55 -0500

    the server is functional except for ahtuentication

--- database.go
+++ database.go
@@ -8,7 +8,7 @@ type DBEntry struct {
 	Name string
 	Hours uint
 	Date time.Time
-	Organization uint
+	Organization string
 	ContactName string
 	ContactEmail string
 }
@@ -16,18 +16,25 @@ type DBEntry struct {
 var _dbEntries = make(map[string][]DBEntry)
 
 /* Adds */
-func DBAdd(email string, entry DBEntry) {
-	_dbEntries[email] = append(_dbEntries[email], entry)
+func DBSet(email string, entry DBEntry, index int) {
+	if index < 0 {
+		_dbEntries[email] = append(_dbEntries[email], entry)
+	} else if index < len(_dbEntries[email]) {
+		_dbEntries[email][index] = entry
+	}
 }
 
 func DBList(email string) []DBEntry {
 	return _dbEntries[email]
 }
 
-func DBGet(email string, index uint) *DBEntry {
-	if uint(len(_dbEntries[email])) > index {
-		return &_dbEntries[email][index]
+func DBGet(email string, index int) DBEntry {
+	if len(_dbEntries[email]) > index && index >= 0 {
+		return _dbEntries[email][index]
 	}
 
-	return nil
+	return DBEntry{
+		Date: time.Now(),
+		Hours: 1,
+	}
 }
--- server.go
+++ server.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"strings"
 	"regexp"
+	"strconv"
+	"time"
 )
 
 type UserData struct {
@@ -14,26 +16,30 @@ type UserData struct {
 	Email string
 }
 
-func getUser(query url.Values) UserData {
-	token := query.Get("token")
-	fmt.Println(token)
-	// todo
+func getUser(_ string) UserData {
+	// TODO
 	return UserData{Name: "Bob", Email: "bob@example.com"}
 }
 
 func process(in []byte, query url.Values) []byte {
 	var re = regexp.MustCompile("(?s)\\[\\[.*?\\]\\]")
-	var user  = UserData{}
-	return re.ReplaceAllFunc(in, func (rawcode []byte) []byte {
-		if len(user.Email) == 0 {
-			user = getUser(query)
-			if len(user.Email) == 0 {
-				return nil
-			}
+	var user = getUser(query.Get("token"))
+	if len(user.Email) == 0 {
+		return nil
+	}
+
+	var entry DBEntry
+	if len(query["entry"]) != 0 {
+		index, err := strconv.Atoi(query.Get("entry"))
+		if err != nil {
+			index = -1
 		}
-	
+		entry = DBGet(user.Email, index)
+	}
+ 	
+	return re.ReplaceAllFunc(in, func (rawcode []byte) []byte {
 		code := string(rawcode[2:len(rawcode)-2])
-		cmd := strings.Split(code, " ")
+		cmd := strings.Fields(code)
 		if len(cmd) < 1 {
 			return nil
 		}
@@ -46,20 +52,41 @@ func process(in []byte, query url.Values) []byte {
 			if cmd[1] == "email" {
 				return []byte(user.Email)
 			}
+			if cmd[1] == "token" {
+				return []byte(query.Get("token"))
+			}
 			return nil
 		case "entry":
 			if len(cmd) != 2 { return nil }
-			
 			if cmd[1] == "name" { 
+				return []byte(entry.Name)
+			}
+			if cmd[1] == "index" { 
+				return []byte(query.Get("entry"))
+			}
+			if cmd[1] == "hours" { 
+				return []byte(fmt.Sprint(entry.Hours))
+			}
+			if cmd[1] == "date" { 
+				return []byte(entry.Date.Format("2006-01-02"))
+			}
+			if cmd[1] == "org" { 
+				return []byte(entry.Organization)
+			}
+			if cmd[1] == "contact.name" { 
+				return []byte(entry.ContactName)
+			}
+			if cmd[1] == "contact.email" { 
+				return []byte(entry.ContactEmail)
 			}
 			return nil
 		case "repeat":
 			html := strings.Trim(code[6:], " \t\n")
 			out := ""
 			for i, entry := range DBList(user.Email) {
-				out += strings.NewReplacer("[index]", fmt.Sprint(i), "[name]", entry.Name).Replace(html)
+				out += strings.NewReplacer("[index]", fmt.Sprint(i), "[name]", entry.Name, "[token]", query.Get("token")).Replace(html)
 			}
-			return nil
+			return []byte(out)
 		default:
 			return nil				
 		}
@@ -97,6 +124,55 @@ func main() {
 
 		w.Header().Set("Content-Type", "text/html")
 		w.Write(process(body, r.URL.Query()))
+	})
+
+	http.HandleFunc("/edit", func (w http.ResponseWriter, r *http.Request) { 
+		body, err := ioutil.ReadFile("edit.html")
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(process(body, r.URL.Query()))
+	})
+
+	http.HandleFunc("/add", func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", "/edit?entry=-1&token=" + r.URL.Query().Get("token"))
+		w.WriteHeader(302)
+	})
+
+	http.HandleFunc("/update", func (w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		user := getUser(query.Get("Token"))
+		if len(user.Email) == 0 {
+			w.WriteHeader(400)
+			return
+		}
+		index, err := strconv.Atoi(query.Get("entry"))
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
+		hours, err := strconv.ParseUint(query.Get("hours"), 10, 64)
+		if err != nil {
+			hours = 1
+		}
+		date, err := time.Parse("2006-01-02", query.Get("date"))
+		if err != nil {
+			date = time.Now()
+		}
+		DBSet(user.Email, DBEntry{
+			Name: query.Get("name"),
+			Hours: uint(hours),
+			Date: date,
+			Organization: query.Get("org"),
+			ContactName: query.Get("contactname"),
+			ContactEmail: query.Get("contactemail"),
+		}, index)
+	
+		w.Header().Set("Location", "/list?token=" + query.Get("token"))
+		w.WriteHeader(302)
 	})
 
 	fmt.Println("http://localhost:8080/");
